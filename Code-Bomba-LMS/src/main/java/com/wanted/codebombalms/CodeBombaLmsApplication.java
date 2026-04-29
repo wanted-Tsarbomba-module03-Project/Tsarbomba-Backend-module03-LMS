4c12e21548955b5b8f06d13272ed91ac86994dd8
Dong_gu <dkdlwkr207@gmail.com>
2026-04-29 14:41:28 +0900

    fist

---
+++ Code-Bomba-LMS/src/main/java/com/wanted/codebombalms/CodeBombaLmsApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.codebombalms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBombaLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeBombaLmsApplication.class, args);
+    }
+
+}
